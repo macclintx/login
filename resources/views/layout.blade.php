6c4ce2fa4be06b92fe3a04c468630fbb11b34804
macclintx <macclintx@gmail.com>
2024-04-30 22:02:50 +0300

    defined route to login page

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -23,70 +23,14 @@
   
       <!-- Collapsible wrapper -->
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <!-- Left links -->
-        <ul class="navbar-nav me-3">
-          <li class="nav-item">
-            <a class="nav-link active d-flex align-items-center" aria-current="page" href="#"><i
-                class="fas fa-bars pe-2"></i>Menu</a>
-          </li>
-        </ul>
-        <!-- Left links -->
+
   
-        <form class="d-flex align-items-center w-100 form-search">
-          <div class="input-group">
-            <button class="btn btn-light dropdown-toggle shadow-0" type="button" data-mdb-toggle="dropdown"
-              aria-expanded="false" style="padding-bottom: 0.4rem;">
-              All
-            </button>
-            <ul class="dropdown-menu dropdown-menu-dark fa-ul">
-              <li>
-                <a class="dropdown-item" href="#"><span class="fa-li pe-2"><i class="fas fa-search"></i></span>All</a>
-              </li>
-              <li>
-                <a class="dropdown-item" href="#"><span class="fa-li pe-2"><i
-                      class="fas fa-film"></i></span>Titles</a>
-              </li>
-              <li>
-                <a class="dropdown-item" href="#"><span class="fa-li pe-2"><i class="fas fa-tv"></i></span>TV
-                  Episodes</a>
-              </li>
-              <li>
-                <a class="dropdown-item" href="#"><span class="fa-li pe-2"><i
-                      class="fas fa-user-friends"></i></span>Celebs</a>
-              </li>
-              <li>
-                <a class="dropdown-item" href="#"><span class="fa-li pe-2"><i
-                      class="fas fa-building"></i></span>Companies</a>
-              </li>
-              <li>
-                <a class="dropdown-item" href="#"><span class="fa-li pe-2"><i
-                      class="fas fa-key"></i></span>Keywords</a>
-              </li>
-              <li>
-                <hr class="dropdown-divider" />
-              </li>
-              <li>
-                <a class="dropdown-item" href="#"><span class="fa-li pe-2"><i
-                      class="fas fa-search-plus"></i></span>Advanced
-                  search<i class="fas fa-chevron-right ps-2"></i></a>
-              </li>
-            </ul>
-            <input type="search" class="form-control" placeholder="Search" aria-label="Search" />
-          </div>
-          <a href="#!" class="text-white"><i class="fas fa-search ps-3"></i></a>
-        </form>
-  
-        <ul class="navbar-nav ms-3">
-          <li class="nav-item me-3">
-            <a class="nav-link d-flex align-items-center" href="#!">IMDbPro</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link d-flex align-items-center me-3" href="#!">
-              <i class="fas fa-bookmark pe-2"></i> Watchlist
-            </a>
+        <ul class="navbar-nav ms-5 align-items-right">
+          <li class="nav-item" style="width: 65px;">
+            <a class="nav-link d-flex align-items-center" href="{{ route('login') }}">Sign In</a>
           </li>
           <li class="nav-item" style="width: 65px;">
-            <a class="nav-link d-flex align-items-center" href="#!">Sign In</a>
+            <a class="nav-link d-flex align-items-center" href="#!">SignUp</a>
           </li>
         </ul>
       </div>
@@ -95,5 +39,9 @@
     <!-- Container wrapper -->
   </nav>
   <!-- Navbar -->
+  <div class="container">
+    <br>
+    @yield('content')
+  </div>
     </body>
 </html>
