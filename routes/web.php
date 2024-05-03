924a991b3442b858d89385ff90963b7526340f95
macclintx <macclintx@gmail.com>
2024-05-03 23:09:11 +0300

    switching to using Laravel Breeze

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('layout');
+    return view('welcome');
 });
-
-Route::get('login', function () {
-    return view('login');
-})->name('login');
